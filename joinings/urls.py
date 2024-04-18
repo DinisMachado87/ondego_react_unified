b6f30793d417d3cafad650c81f71e1326ad78254
Dinis Machado <dinisma@gmail.com>
2024-04-18 22:19:06 +0000

    Debug Joining urls

--- joinings/urls.py
+++ joinings/urls.py
@@ -3,6 +3,6 @@ from joinings import views
 
 
 urlpatterns = [
-    path('joinings', views.JoiningList.as_view()),
+    path('joinings/', views.JoiningList.as_view()),
     path('joinings/<int:pk>/', views.JoiningDetail.as_view()),
 ]
