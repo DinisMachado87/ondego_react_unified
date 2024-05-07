71d2585c545fe2fb5fa847d40434c4104e0571a7
Dinis Machado <dinisma@gmail.com>
2024-05-07 22:38:11 +0000

    Add comment filter by event

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,16 @@ from rest_framework import generics, permissions
 from ondego_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializer import CommentSerializer, CommentDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['event']
+    
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -17,3 +21,5 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentDetailSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['event']
